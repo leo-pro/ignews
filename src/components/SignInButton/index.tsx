cbb21d9b8b01f4bddc3c511ddec6a9efbd90dfeb
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-09-22 20:47:55 -0300

    Next.js Configuration, Header and SignInButton finished

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,23 @@
+import styles from "./styles.module.scss";
+
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const isUserLoggedIn = false;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      {" "}
+      <FaGithub color="#eba417" />
+      Leonardo Alves
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      {" "}
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
