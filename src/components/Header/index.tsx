47838da5b9e43fd680f209038030ba7bbee08290
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-10-29 15:37:48 -0300

    feat: listing all post on page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,9 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
@@ -7,8 +11,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ignews" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
